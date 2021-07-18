e933b90175342c322703f62946fca343b0e4fd3d
aldilaputrihartati <86336710+aldilaputrihartati@users.noreply.github.com>
2021-07-18 23:05:30 +0700

    Add files via upload

---
+++ config.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package form;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Asus
+ */
+class config {
+
+    static Connection configDB() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
